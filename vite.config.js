100e528f653ac55e7c4e6ce4899040864f3c250f
Zuzanna Krętkowska <z.kretkow@gmail.com>
2025-06-22 20:45:14 +0200

    Popraw base i ścieżki

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: '/lab10/', 
+  base: '/lab10/', // <-- to jest kluczowe!
   plugins: [
     tailwindcss(),
   ],
