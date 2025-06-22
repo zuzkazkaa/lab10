5f5c106ffb88e9ff2cb619390561ddd7d091cb0e
Zuzanna Krętkowska <z.kretkow@gmail.com>
2025-06-22 15:46:32 +0200

    Initial commit - cały projekt z workflow

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './style.css'
+
+document.getElementById('app').innerHTML = `
+  <h1 class="text-2xl font-bold text-primary">Tytuł strony</h1>
+  <button class="bg-blue-300 p-4">count</button>
+`;
